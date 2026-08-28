691757665fffb11f76754221efc93ef48aa92681
Wawrek Deighan <W.Deighan@albynschool.org>
2026-08-28 11:45:01 +0100

    000002

--- Worksheet-4/worksheet-4_task-12a.py
+++ Worksheet-4/worksheet-4_task-12a.py
@@ -2,70 +2,70 @@ print("10-Questions.com v1.0.0")
 print("All answers should be capitalised with spaces between words, if necessary.")
 
 print("Q1: What is the capital of France?")
-q1_answer = input()
+q1_answer = input().upper()
 if q1_answer == "PARIS":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q2: What is the largest planet in our solar system?")
-q2_answer = input()
+q2_answer = input().upper()
 if q2_answer == "JUPITER":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q3: What is the chemical symbol for water?")
-q3_answer = input()
+q3_answer = input().upper()
 if q3_answer == "H2O":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q4: Who wrote the play 'Romeo and Juliet'? (First and last name)")
-q4_answer = input()
+q4_answer = input().upper()
 if q4_answer == "WILLIAM SHAKESPEARE":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q5: What is the capital of Japan?")
-q5_answer = input()
+q5_answer = input().upper()
 if q5_answer == "TOKYO":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q6: What is the largest mammal in the world?")
-q6_answer = input()
+q6_answer = input().upper()
 if q6_answer == "BLUE WHALE":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q7: What is the chemical symbol for gold?")
-q7_answer = input()
+q7_answer = input().upper()
 if q7_answer == "AU":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q8: Who painted the Mona Lisa? (First and last name)")
-q8_answer = input()
+q8_answer = input().upper()
 if q8_answer == "LEONARDO DA VINCI":
     print("Correct!")
 else:
     print("Incorrect!")
 
 print("Q9: What is the capital of Australia?")
-q9_answer = input()
+q9_answer = input().upper()
 if q9_answer == "CANBERRA":
     print("Correct!")
 else:
     print("Incorrect!")             
 
 print("Q10: What is the smallest prime number?")
-q10_answer = input()
+q10_answer = input().upper()
 if q10_answer == "2":
     print("Correct!")
 else:
---
+++ Worksheet-4/worksheet-4_task-12c.py
@@ -0,0 +1,25 @@
+# Speed Calculator v1.0.0 
+print("Speed Calculator v1.0.0")
+print("What is the distance travelled (in metres)?")
+distance = int(input())
+print("What is the time taken (in seconds)?")
+time = int(input())
+speed = distance / time
+rounded_speed = round(speed, 2)
+print("The speed of the object is ", rounded_speed," metres per second.")
+
+# Average Score Calculator v1.0.0
+print("Average Score Calculator v1.0.0")
+maths_score = int(input("What is your score in Maths? (percentage) "))
+english_score = int(input("What is your score in English? (percentage) "))
+computing_score = int(input("What is your score in Computing? (percentage) "))
+average_score = (maths_score + english_score + computing_score) / 3
+rounded_average = round(average_score, 2)
+print("Your average score is " + str(rounded_average) + "%.")
+
+# Percentage Calculator v1.0.0
+print("Percentage Calculator v1.0.0")
+original_price = int(input("What is the original price of the item? (in pounds) "))
+discount = original_price * 0.20
+final_price = original_price - discount
+print("The final price of the item after the discount is " + str(final_price) + " pounds.")
