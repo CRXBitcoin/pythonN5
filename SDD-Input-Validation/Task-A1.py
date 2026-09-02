169d65b0483172c8aae88fe756d57f55581f39ab
Conor Deighan <conordeighan@Conors-MacBook-Pro.local>
2026-09-02 18:11:41 +0100

    000006

--- SDD-Input-Validation/Task-A1.py
+++
@@ -1,5 +0,0 @@
-test_score = [""]
-test_score = int(input("Please enter your test score (0-100): "))
-while test_score < 0 or test_score > 100:
-    print("Invalid input. Please enter a score between 0 and 100.")
-    test_score = int(input("Please enter your test score (0-100): "))
